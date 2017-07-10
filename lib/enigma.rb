b4d70efc3d0f1a2366b851f4ad2dcc4308dc9f82
snayrouz <samuel.nayrouz@gmail.com>
2017-07-10 16:34:40 -0600

    Make updates

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -14,30 +14,6 @@ class Enigma
     Time.now.strftime("%d%m%y").to_i
   end
 
-  def rand_key
-    key = ""
-    5.times {key << rand(0..9).to_s}
-    key.to_i
-    return key
-  end
-
-<<<<<<< HEAD
-  def offset(date)
-=======
-  def rotations(key)
-    (0..3).map do |num|
-      key.to_s[num, 2].to_i
-    end
-  end
-
-  def offset(date = (Date.today.strftime "%d%m%y"))
->>>>>>> 052b41c6639a22f3edef1b5aee359eb1f6fd7e04
-    squared_date = (date.to_i**2)
-    squared_date.to_s.split("")[-4..-1].map do |num|
-      num.to_i
-    end
-  end
-
   def combined(rotations,offset)
 
     # iterate through the message and assign each message character to a key value
@@ -50,14 +26,8 @@ class Enigma
 
 end
 
-
 # e = Enigma.new
 # e.rand_key
-
-
-# at some point, key = nil. default variable? key = nil ? key = Key.new
-# make a Key class?
-
 
 # def read_message(read_name)
 #   (File.open(read_name, 'r') { |f| f.read}).chomp
@@ -66,52 +36,6 @@ end
 # def write_message(message, write_name)
 #   File.open(write_name, 'w') {|f| f.write(message)}
 # end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# key = "12345"
-# date = Date.today
-
-
-  # not sure if this method will sit on this class
-
-
-#   my_message.split.map do | characters |
-#     characters.
-#     # need to
-#
-#   encrypted_message =
-#   output
-# end
-#
-#
-# def encrypt(my_message, key = rand_key, date = (Date.today.strftime "%d%m%y"))
-#
-#
-#
-# end
-#
 
 #
 # def read_message(read_name)
